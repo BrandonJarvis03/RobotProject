a83e0b5dc30567178ee4c9107e44436f41720b44
BrandonJarvis03 <brandonjarvis03@gmail.com>
2021-07-20 10:26:45 -0230

    Added rotations

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ r = robot.RobotController()
 r.connect()
 
 r.forward(350)
-r.left(100)
+r.rotate_counterclockwise(90)
+r.forward(100)
+r.rotate_clockwise(90)
 # Room 1 Code:
 m = r.read_marker()
 print(f"Marker Number: {m}")
 if m == 1:
-    r.left(700)
+    r.rotate_counterclockwise(90)
+    r.forward(700)
+    r.rotate_clockwise(90)
     t1 = r.take_temperature()
     print(f"Tempature in Room: {t1}")
     r.forward(110)
@@ -16,12 +20,19 @@ if m == 1:
     print(f"Is a person there?: {n}")
     if n == True:
         r.rescue_person()
-        r.backward(110)
-        r.right(800)
-        r.backward(350)
+        r.rotate_clockwise(180)
+        r.forward(110)
+        r.rotate_counterclockwise(90)
+        r.forward(800)
+        r.rotate_clockwise(90)
+        r.forward(350)
+        r.rotate_counterclockwise(180)
     if n == False:
-        r.backward(110)
-        r.right(800)
+        r.rotate_clockwise(180)
+        r.forward(110)
+        r.rotate_counterclockwise(90)
+        r.forward(700)
+        r.rotate_counterclockwise(90)
 if m == 2:
     t1 = 0
 temperature_list = [t1,]
